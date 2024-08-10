2e759f87022c407d9a59dd9ac5cd3b96f4cd43c8
samuxka <samukactto@gmail.com>
2024-08-10 16:23:39 -0300

    fsfssf

--- LacosDeRepeticao/Exercicio.java
+++ LacosDeRepeticao/Exercicio.java
@@ -4,7 +4,7 @@ public class Exercicio {
     public static void main(String[] args) {
         //Imprimir numeros pares de 0 a 100
         //Imprimir numeros primos 
-        for(int numPrimo=2;numPrimo<=1000000;numPrimo++){
+        for(int numPrimo=2;numPrimo<=1000000000;numPrimo++){
             boolean ehPrimo = true;
             for(int i=2;i<=Math.sqrt(numPrimo);i++){
                 if(numPrimo%i==0){
